a862eeaf66ab458a1397d608f8d7a87fc574c85a
2060609119@qq.com <2060906119@qq.com>
2022-09-04 17:59:55 +0800

    提交vue部分

--- src/main/java/com/tongxin/system/controller/HelloController.java
+++ src/main/java/com/tongxin/system/controller/HelloController.java
@@ -1,9 +1,12 @@
 package com.tongxin.system.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static cn.hutool.core.lang.Console.log;
 
 /**
  * @Description:
@@ -14,11 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping
+@Slf4j
 public class HelloController {
 
     @GetMapping("/index")
-    public String hello(@RequestBody RequestBody body) {
-
+    public String hello() {
         return "hello";
     }
 
